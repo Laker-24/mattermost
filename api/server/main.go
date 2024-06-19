881682c6c30b7e93696339fdc0a71cde779010a8
Laker-24 <2722039868@qq.com>
2024-06-19 17:24:15 +0800

    再次修改

--- api/server/main.go
+++ api/server/main.go
@@ -11,6 +11,7 @@ import (
 	"go/printer"
 	"go/token"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/Laker-24/mattermost/tree/master/server/channels/api4"
 	"github.com/pb33f/libopenapi"
 	v3high "github.com/pb33f/libopenapi/datamodel/high/v3"
