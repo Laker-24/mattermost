be74d890a0f08fa9c952fcbaaa619e83e771170a
Laker-24 <2722039868@qq.com>
2024-06-20 18:01:45 +0800

    修改

--- webapp/channels/src/entry.tsx
+++ webapp/channels/src/entry.tsx
@@ -20,7 +20,6 @@ import 'katex/dist/katex.min.css';
 import '@mattermost/compass-icons/css/compass-icons.css';
 import '@mattermost/components/dist/index.esm.css';
 
-import mysql from 'mysql2/promise';
 declare global {
     interface Window {
         publicPath?: string;
@@ -79,35 +78,5 @@ appendOnDOMContentLoadedEvent(() => {
     preRenderSetup(renderReactRootComponent);
 });
 
-// 配置数据库连接
-const dbConfig = {
-    host: '101.43.103.236:3307',
-    user: 'mmuser',
-    password: 'mmuser_password',
-    database: 'mattermost'
-};
 
-// 定义删除超过24小时记录的函数
-async function deleteOldPosts() {
-    const connection = await mysql.createConnection(dbConfig);
-    try {
-        const [rows, fields] = await connection.execute(`
-            DELETE FROM Posts WHERE CreateAt < ?
-        `, [Date.now() * 1000 - 24 * 60 * 60 * 1000 * 1000]);
 
-        console.log(`Deleted ${rows.affectedRows} old posts`);
-    } catch (err) {
-        console.error('Failed to delete old posts:', err);
-    } finally {
-        await connection.end();
-    }
-}
-
-// 定时器函数，每隔5分钟执行一次
-function scheduleDelete() {
-    deleteOldPosts();
-    setInterval(deleteOldPosts, 5 * 60 * 1000); // 每5分钟执行一次
-}
-
-// 组件挂载时自动执行
-scheduleDelete();
